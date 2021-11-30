1e563edc84fe4cd135d7f95a90dafbffc0b69df5
Jesus Sanchez <sanchez.moral.jesus@gmail.com>
2021-11-30 18:08:35 +0100

    Change max value for price and ram

--- api/models/Phone.js
+++ api/models/Phone.js
@@ -7,11 +7,11 @@ const PhoneSchema = new Schema({
         "manufacturer": {type: String, require: true, min: 3, max: 20},
         "description": {type: String, require: true, min: 3, max: 300},
         "color": {type: String, require: true, min: 3, max: 20},
-        "price": {type: Number, require: true, min: 2, max: 4},
+        "price": {type: Number, require: true, min: 2, max: 2024},
         "imageFileName": {type: String, require: true, min: 3, max: 20},
         "screen": {type: String, require: true, min: 3, max: 60},
         "processor": {type: String, require: true, min: 3, max: 60},
-        "ram": {type: Number, require: true, min: 1, max: 3},
+        "ram": {type: Number, require: true, min: 1, max: 128}
 })
 
 module.exports = model("Phone", PhoneSchema)
