ea04a57584f1ebc75340e85c34237fe42ededb08
Jesus Sanchez <sanchez.moral.jesus@gmail.com>
2021-12-02 16:40:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,9 +25,13 @@ app.use("/phones", phoneRouter)
 //Server static fields if in production
 if (Prod === "production") {
     //Create static folder
-    app.use(express.static("client/build"))
+    app.use(express.static(path.join(__dirname, '/client/build')))
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}else{
+    app.get("/", (req,res) => {
+        res.send("Api running")
     })
 }
 
